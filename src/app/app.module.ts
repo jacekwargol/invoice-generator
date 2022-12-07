cea9094df82119071d78508e1acafa6a5b49a4f5
Jacek Wargol <jwargol@gmail.com>
2022-12-07 23:33:33 +0100

    Add json-server package to mock api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddInvoiceComponent } from './components/invoice/add-invoice/add-invoice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddInvoiceComponent
   ],
   imports: [
     BrowserModule
