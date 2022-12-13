89f54e20bb8ecaa7d9e7cab84e0a03d98dcc4fe9
Jacek Wargol <jwargol@gmail.com>
2022-12-13 20:48:53 +0100

    Add responsive sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'invoice-generator';
+export class AppComponent implements OnInit {
+  title = 'Invoice Generator';
+  mobileMode = false;
+
+  constructor(private breakpoint: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    this.breakpoint.observe([Breakpoints.Small, Breakpoints.XSmall]).subscribe(
+      result => {
+        this.mobileMode = false;
+        if (result.matches) {
+          this.mobileMode = true;
+        }
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { AppComponent } from './app.component';
 import { AddInvoiceComponent } from './components/invoice/add-invoice/add-invoice.component';
@@ -31,7 +35,10 @@ import { CompanyComponent } from './components/company/company/company.component
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
